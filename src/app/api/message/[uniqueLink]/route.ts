35fc2500807d5f7480663d98feb69454bc6569ac
nattzchukwumela <chukwumelapraise@gmail.com>
2025-09-03 07:38:58 +0100

    api route for user unique link messages

---
+++ src/app/api/message/[uniqueLink]/route.ts
@@ -0,0 +1,43 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { uniqueLink: string } },
+) {
+  try {
+    const { data } = await req.json();
+
+    // check for user
+    const user = await prisma.user.findUnique({
+      where: { uniqueLink: params.uniqueLink },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, err: "User not found" },
+        { status: 404 },
+      );
+    }
+
+    // create message
+    const message = await prisma.anonymousMessage.create({
+      data: {
+        receiverId: user.id,
+        text: data.text,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Message sent successfully",
+      data: message,
+    });
+  } catch (err) {
+    console.error("Error creating message:", err);
+    return NextResponse.json(
+      { success: false, err: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
