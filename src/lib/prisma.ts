e8b6eabfeb2dd8e8bba97704e99b7d0fcb9adbd0
nattzchukwumela <chukwumelapraise@gmail.com>
2026-04-24 19:27:51 +0100

    Codebase refactoring and update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: { rejectUnauthorized: false },
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
